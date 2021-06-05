45436ecfa01038e1b57a6513d9b38b5dde617616
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-05 20:27:21 +0530

    cleardb crditial update cratepoll methode

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   host: "us-cdbr-east-04.cleardb.com",
   user: "bc6c5983a04300",
   password: "cf2557be",
   database:"heroku_32db7ec924b81f2"
   });
-  connection.connect(function(err) {
+  /*connection.connect(function(err) {
     if (err) throw err;
-  });
+  });*/
   module.exports = connection;
