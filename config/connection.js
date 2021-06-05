96bdc2bc83bb87a7feb19d9bcc5d65df08db3a71
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-05 20:26:11 +0530

    cleardb crditial update 2

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  HOST: "us-cdbr-east-04.cleardb.com",
-  USER: "bc6c5983a04300",
-  PASSWORD: "cf2557be",
-  DB:"heroku_32db7ec924b81f2"
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "bc6c5983a04300",
+  password: "cf2557be",
+  database:"heroku_32db7ec924b81f2"
   });
   connection.connect(function(err) {
     if (err) throw err;
