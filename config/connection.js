c762a225d80fce2c38a43ae57999f84fb2682f90
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-08 22:17:07 +0530

    updated data

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database:"burgers_db"
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "bc6c5983a04300",
+    password: "cf2557be",
+    database:"heroku_32db7ec924b81f2"
   });
   connection.connect(function(err) {
     if (err) throw err;
