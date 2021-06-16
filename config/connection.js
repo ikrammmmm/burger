ae1e05a8273ec0a55931ba964afb772f3137369c
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-16 12:43:46 +0530

    fixing heroku crash

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+
+  function databaseconnect()
+  {
+    var connection = mysql.createConnection({
     host: "us-cdbr-east-04.cleardb.com",
     user: "bc6c5983a04300",
     password: "cf2557be",
     database:"heroku_32db7ec924b81f2"
   });
   connection.connect(function(err) {
-    if (err) throw err;
+    if (err) databaseconnect();
   });
+
+  }
+  databaseconnect()
   module.exports = connection;
