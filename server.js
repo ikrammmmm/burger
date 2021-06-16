3081f3dc70a360eedb2ed5e357910b4c46e072fe
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-16 11:55:17 +0530

    fixing heroku crash

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.use(express.static(process.cwd() + "/public"));
 var appPort = process.env.PORT || 3000;
 
 
-app.listen(appPort);
+app.listen(appPort, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
